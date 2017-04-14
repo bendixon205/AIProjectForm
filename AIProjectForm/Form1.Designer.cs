a53b4f803c6f18b7290fae79d1821c9a5e061e5b
Ben Dixon <btdixon205@gmail.com>
2017-04-14 11:28:12 -0400

    2 array implementation

---
+++ AIProjectForm/Form1.Designer.cs
@@ -0,0 +1,439 @@
+﻿namespace AIProjectForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnL0 = new System.Windows.Forms.Button();
+            this.btnL1 = new System.Windows.Forms.Button();
+            this.btnL2 = new System.Windows.Forms.Button();
+            this.btnL3 = new System.Windows.Forms.Button();
+            this.btnL4 = new System.Windows.Forms.Button();
+            this.btnL5 = new System.Windows.Forms.Button();
+            this.btnR0 = new System.Windows.Forms.Button();
+            this.btnR1 = new System.Windows.Forms.Button();
+            this.btnR2 = new System.Windows.Forms.Button();
+            this.btnR3 = new System.Windows.Forms.Button();
+            this.btnR4 = new System.Windows.Forms.Button();
+            this.btnR5 = new System.Windows.Forms.Button();
+            this.lblL0 = new System.Windows.Forms.Label();
+            this.lblL1 = new System.Windows.Forms.Label();
+            this.lblL2 = new System.Windows.Forms.Label();
+            this.lblL3 = new System.Windows.Forms.Label();
+            this.lblL4 = new System.Windows.Forms.Label();
+            this.lblL5 = new System.Windows.Forms.Label();
+            this.lblR0 = new System.Windows.Forms.Label();
+            this.lblR1 = new System.Windows.Forms.Label();
+            this.lblR2 = new System.Windows.Forms.Label();
+            this.lblR3 = new System.Windows.Forms.Label();
+            this.lblR4 = new System.Windows.Forms.Label();
+            this.lblR5 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblBottomStore = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblTopStore = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTurn = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnL0
+            // 
+            this.btnL0.Location = new System.Drawing.Point(49, 46);
+            this.btnL0.Name = "btnL0";
+            this.btnL0.Size = new System.Drawing.Size(46, 23);
+            this.btnL0.TabIndex = 0;
+            this.btnL0.TabStop = false;
+            this.btnL0.Text = "Select";
+            this.btnL0.UseVisualStyleBackColor = true;
+            // 
+            // btnL1
+            // 
+            this.btnL1.Location = new System.Drawing.Point(49, 75);
+            this.btnL1.Name = "btnL1";
+            this.btnL1.Size = new System.Drawing.Size(46, 23);
+            this.btnL1.TabIndex = 1;
+            this.btnL1.TabStop = false;
+            this.btnL1.Text = "Select";
+            this.btnL1.UseVisualStyleBackColor = true;
+            // 
+            // btnL2
+            // 
+            this.btnL2.Location = new System.Drawing.Point(49, 104);
+            this.btnL2.Name = "btnL2";
+            this.btnL2.Size = new System.Drawing.Size(46, 23);
+            this.btnL2.TabIndex = 2;
+            this.btnL2.TabStop = false;
+            this.btnL2.Text = "Select";
+            this.btnL2.UseVisualStyleBackColor = true;
+            // 
+            // btnL3
+            // 
+            this.btnL3.Location = new System.Drawing.Point(49, 133);
+            this.btnL3.Name = "btnL3";
+            this.btnL3.Size = new System.Drawing.Size(46, 23);
+            this.btnL3.TabIndex = 3;
+            this.btnL3.TabStop = false;
+            this.btnL3.Text = "Select";
+            this.btnL3.UseVisualStyleBackColor = true;
+            // 
+            // btnL4
+            // 
+            this.btnL4.Location = new System.Drawing.Point(49, 162);
+            this.btnL4.Name = "btnL4";
+            this.btnL4.Size = new System.Drawing.Size(46, 23);
+            this.btnL4.TabIndex = 4;
+            this.btnL4.TabStop = false;
+            this.btnL4.Text = "Select";
+            this.btnL4.UseVisualStyleBackColor = true;
+            // 
+            // btnL5
+            // 
+            this.btnL5.Location = new System.Drawing.Point(49, 191);
+            this.btnL5.Name = "btnL5";
+            this.btnL5.Size = new System.Drawing.Size(46, 23);
+            this.btnL5.TabIndex = 5;
+            this.btnL5.TabStop = false;
+            this.btnL5.Text = "Select";
+            this.btnL5.UseVisualStyleBackColor = true;
+            // 
+            // btnR0
+            // 
+            this.btnR0.Location = new System.Drawing.Point(49, 191);
+            this.btnR0.Name = "btnR0";
+            this.btnR0.Size = new System.Drawing.Size(46, 23);
+            this.btnR0.TabIndex = 11;
+            this.btnR0.TabStop = false;
+            this.btnR0.Text = "Select";
+            this.btnR0.UseVisualStyleBackColor = true;
+            // 
+            // btnR1
+            // 
+            this.btnR1.Location = new System.Drawing.Point(49, 162);
+            this.btnR1.Name = "btnR1";
+            this.btnR1.Size = new System.Drawing.Size(46, 23);
+            this.btnR1.TabIndex = 10;
+            this.btnR1.TabStop = false;
+            this.btnR1.Text = "Select";
+            this.btnR1.UseVisualStyleBackColor = true;
+            // 
+            // btnR2
+            // 
+            this.btnR2.Location = new System.Drawing.Point(49, 133);
+            this.btnR2.Name = "btnR2";
+            this.btnR2.Size = new System.Drawing.Size(46, 23);
+            this.btnR2.TabIndex = 9;
+            this.btnR2.TabStop = false;
+            this.btnR2.Text = "Select";
+            this.btnR2.UseVisualStyleBackColor = true;
+            // 
+            // btnR3
+            // 
+            this.btnR3.Location = new System.Drawing.Point(49, 104);
+            this.btnR3.Name = "btnR3";
+            this.btnR3.Size = new System.Drawing.Size(46, 23);
+            this.btnR3.TabIndex = 8;
+            this.btnR3.TabStop = false;
+            this.btnR3.Text = "Select";
+            this.btnR3.UseVisualStyleBackColor = true;
+            // 
+            // btnR4
+            // 
+            this.btnR4.Location = new System.Drawing.Point(49, 75);
+            this.btnR4.Name = "btnR4";
+            this.btnR4.Size = new System.Drawing.Size(46, 23);
+            this.btnR4.TabIndex = 7;
+            this.btnR4.TabStop = false;
+            this.btnR4.Text = "Select";
+            this.btnR4.UseVisualStyleBackColor = true;
+            // 
+            // btnR5
+            // 
+            this.btnR5.Location = new System.Drawing.Point(49, 46);
+            this.btnR5.Name = "btnR5";
+            this.btnR5.Size = new System.Drawing.Size(46, 23);
+            this.btnR5.TabIndex = 6;
+            this.btnR5.TabStop = false;
+            this.btnR5.Text = "Select";
+            this.btnR5.UseVisualStyleBackColor = true;
+            // 
+            // lblL0
+            // 
+            this.lblL0.AutoSize = true;
+            this.lblL0.Location = new System.Drawing.Point(111, 51);
+            this.lblL0.Name = "lblL0";
+            this.lblL0.Size = new System.Drawing.Size(13, 13);
+            this.lblL0.TabIndex = 12;
+            this.lblL0.Text = "4";
+            this.lblL0.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblL1
+            // 
+            this.lblL1.AutoSize = true;
+            this.lblL1.Location = new System.Drawing.Point(111, 80);
+            this.lblL1.Name = "lblL1";
+            this.lblL1.Size = new System.Drawing.Size(13, 13);
+            this.lblL1.TabIndex = 13;
+            this.lblL1.Text = "4";
+            this.lblL1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblL2
+            // 
+            this.lblL2.AutoSize = true;
+            this.lblL2.Location = new System.Drawing.Point(111, 109);
+            this.lblL2.Name = "lblL2";
+            this.lblL2.Size = new System.Drawing.Size(13, 13);
+            this.lblL2.TabIndex = 14;
+            this.lblL2.Text = "4";
+            this.lblL2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblL3
+            // 
+            this.lblL3.AutoSize = true;
+            this.lblL3.Location = new System.Drawing.Point(111, 138);
+            this.lblL3.Name = "lblL3";
+            this.lblL3.Size = new System.Drawing.Size(13, 13);
+            this.lblL3.TabIndex = 15;
+            this.lblL3.Text = "4";
+            this.lblL3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblL4
+            // 
+            this.lblL4.AutoSize = true;
+            this.lblL4.Location = new System.Drawing.Point(111, 167);
+            this.lblL4.Name = "lblL4";
+            this.lblL4.Size = new System.Drawing.Size(13, 13);
+            this.lblL4.TabIndex = 16;
+            this.lblL4.Text = "4";
+            this.lblL4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblL5
+            // 
+            this.lblL5.AutoSize = true;
+            this.lblL5.Location = new System.Drawing.Point(111, 196);
+            this.lblL5.Name = "lblL5";
+            this.lblL5.Size = new System.Drawing.Size(13, 13);
+            this.lblL5.TabIndex = 17;
+            this.lblL5.Text = "4";
+            this.lblL5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblR0
+            // 
+            this.lblR0.AutoSize = true;
+            this.lblR0.Location = new System.Drawing.Point(19, 196);
+            this.lblR0.Name = "lblR0";
+            this.lblR0.Size = new System.Drawing.Size(13, 13);
+            this.lblR0.TabIndex = 23;
+            this.lblR0.Text = "4";
+            this.lblR0.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblR1
+            // 
+            this.lblR1.AutoSize = true;
+            this.lblR1.Location = new System.Drawing.Point(19, 167);
+            this.lblR1.Name = "lblR1";
+            this.lblR1.Size = new System.Drawing.Size(13, 13);
+            this.lblR1.TabIndex = 22;
+            this.lblR1.Text = "4";
+            this.lblR1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblR2
+            // 
+            this.lblR2.AutoSize = true;
+            this.lblR2.Location = new System.Drawing.Point(19, 138);
+            this.lblR2.Name = "lblR2";
+            this.lblR2.Size = new System.Drawing.Size(13, 13);
+            this.lblR2.TabIndex = 21;
+            this.lblR2.Text = "4";
+            this.lblR2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblR3
+            // 
+            this.lblR3.AutoSize = true;
+            this.lblR3.Location = new System.Drawing.Point(19, 109);
+            this.lblR3.Name = "lblR3";
+            this.lblR3.Size = new System.Drawing.Size(13, 13);
+            this.lblR3.TabIndex = 20;
+            this.lblR3.Text = "4";
+            this.lblR3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblR4
+            // 
+            this.lblR4.AutoSize = true;
+            this.lblR4.Location = new System.Drawing.Point(19, 80);
+            this.lblR4.Name = "lblR4";
+            this.lblR4.Size = new System.Drawing.Size(13, 13);
+            this.lblR4.TabIndex = 19;
+            this.lblR4.Text = "4";
+            this.lblR4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblR5
+            // 
+            this.lblR5.AutoSize = true;
+            this.lblR5.Location = new System.Drawing.Point(19, 51);
+            this.lblR5.Name = "lblR5";
+            this.lblR5.Size = new System.Drawing.Size(13, 13);
+            this.lblR5.TabIndex = 18;
+            this.lblR5.Text = "4";
+            this.lblR5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblBottomStore);
+            this.panel1.Controls.Add(this.btnL0);
+            this.panel1.Controls.Add(this.btnL1);
+            this.panel1.Controls.Add(this.btnL2);
+            this.panel1.Controls.Add(this.btnL3);
+            this.panel1.Controls.Add(this.btnL4);
+            this.panel1.Controls.Add(this.btnL5);
+            this.panel1.Controls.Add(this.lblL0);
+            this.panel1.Controls.Add(this.lblL5);
+            this.panel1.Controls.Add(this.lblL1);
+            this.panel1.Controls.Add(this.lblL4);
+            this.panel1.Controls.Add(this.lblL2);
+            this.panel1.Controls.Add(this.lblL3);
+            this.panel1.Location = new System.Drawing.Point(2, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(140, 261);
+            this.panel1.TabIndex = 24;
+            // 
+            // lblBottomStore
+            // 
+            this.lblBottomStore.AutoSize = true;
+            this.lblBottomStore.Location = new System.Drawing.Point(121, 226);
+            this.lblBottomStore.Name = "lblBottomStore";
+            this.lblBottomStore.Size = new System.Drawing.Size(13, 13);
+            this.lblBottomStore.TabIndex = 18;
+            this.lblBottomStore.Text = "0";
+            this.lblBottomStore.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lblTopStore);
+            this.panel2.Controls.Add(this.lblR5);
+            this.panel2.Controls.Add(this.btnR5);
+            this.panel2.Controls.Add(this.lblR0);
+            this.panel2.Controls.Add(this.btnR4);
+            this.panel2.Controls.Add(this.lblR1);
+            this.panel2.Controls.Add(this.btnR3);
+            this.panel2.Controls.Add(this.lblR2);
+            this.panel2.Controls.Add(this.btnR2);
+            this.panel2.Controls.Add(this.lblR3);
+            this.panel2.Controls.Add(this.btnR1);
+            this.panel2.Controls.Add(this.lblR4);
+            this.panel2.Controls.Add(this.btnR0);
+            this.panel2.Location = new System.Drawing.Point(142, 27);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(140, 261);
+            this.panel2.TabIndex = 25;
+            // 
+            // lblTopStore
+            // 
+            this.lblTopStore.AutoSize = true;
+            this.lblTopStore.Location = new System.Drawing.Point(6, 23);
+            this.lblTopStore.Name = "lblTopStore";
+            this.lblTopStore.Size = new System.Drawing.Size(13, 13);
+            this.lblTopStore.TabIndex = 19;
+            this.lblTopStore.Text = "0";
+            this.lblTopStore.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "TURN:";
+            // 
+            // lblTurn
+            // 
+            this.lblTurn.AutoSize = true;
+            this.lblTurn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTurn.Location = new System.Drawing.Point(161, 9);
+            this.lblTurn.Name = "lblTurn";
+            this.lblTurn.Size = new System.Drawing.Size(29, 13);
+            this.lblTurn.TabIndex = 27;
+            this.lblTurn.Text = "Left";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 287);
+            this.Controls.Add(this.lblTurn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Mancala";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnL0;
+        private System.Windows.Forms.Button btnL1;
+        private System.Windows.Forms.Button btnL2;
+        private System.Windows.Forms.Button btnL3;
+        private System.Windows.Forms.Button btnL4;
+        private System.Windows.Forms.Button btnL5;
+        private System.Windows.Forms.Button btnR0;
+        private System.Windows.Forms.Button btnR1;
+        private System.Windows.Forms.Button btnR2;
+        private System.Windows.Forms.Button btnR3;
+        private System.Windows.Forms.Button btnR4;
+        private System.Windows.Forms.Button btnR5;
+        private System.Windows.Forms.Label lblL0;
+        private System.Windows.Forms.Label lblL1;
+        private System.Windows.Forms.Label lblL2;
+        private System.Windows.Forms.Label lblL3;
+        private System.Windows.Forms.Label lblL4;
+        private System.Windows.Forms.Label lblL5;
+        private System.Windows.Forms.Label lblR0;
+        private System.Windows.Forms.Label lblR1;
+        private System.Windows.Forms.Label lblR2;
+        private System.Windows.Forms.Label lblR3;
+        private System.Windows.Forms.Label lblR4;
+        private System.Windows.Forms.Label lblR5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblBottomStore;
+        private System.Windows.Forms.Label lblTopStore;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTurn;
+    }
+}
+
---
+++ AIProjectForm/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AIProjectForm
+{
+    public partial class Form1 : Form
+    {
+        int[] LeftSideVals = { 4, 4, 4, 4, 4, 4 };
+        int[] RightSideVals = { 4, 4, 4, 4, 4, 4 };
+        Label[] LeftSide = new Label[6];
+        Label[] RightSide = new Label[6];
+
+        int TopStore = 0;
+        int BottomStore = 0;
+
+        bool LeftTurn;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Randomize initial turn
+            Random rand = new Random();
+            LeftTurn = true; // Convert.ToBoolean(rand.Next() % 2);
+
+            #region DefineLabelArrays
+            LeftSide[0] = lblL0;
+            LeftSide[1] = lblL1;
+            LeftSide[2] = lblL2;
+            LeftSide[3] = lblL3;
+            LeftSide[4] = lblL4;
+            LeftSide[5] = lblL5;
+
+            RightSide[0] = lblR0;
+            RightSide[1] = lblR1;
+            RightSide[2] = lblR2;
+            RightSide[3] = lblR3;
+            RightSide[4] = lblR4;
+            RightSide[5] = lblR5;
+            #endregion
+
+            MoveStones(1, true);
+            UpdateBoard();
+        }
+
+        bool GameOver()
+        {
+            return (LeftSideVals.Sum() == 0 || RightSideVals.Sum() == 0) ? true : false;
+        }
+        void CaptureStones(int loc, bool left)
+        {
+            int opposite = 5 - loc;
+            
+            if (left == true)
+            {
+                if (LeftSideVals[loc] == 0 && RightSideVals[opposite] > 0)
+                {
+                    BottomStore += LeftSideVals[loc] + RightSideVals[opposite];
+                    LeftSideVals[loc] = 0;
+                    RightSideVals[opposite] = 0;
+                }
+            }
+            else
+            {
+                if (RightSideVals[loc] == 0 && LeftSideVals[opposite] > 0)
+                {
+                    TopStore += RightSideVals[loc] + LeftSideVals[opposite];
+                    RightSideVals[loc] = 0;
+                    LeftSideVals[opposite] = 0;
+                }
+            }
+        }
+        void UpdateBoard()
+        {
+            if (GameOver())
+            {
+                CollectAll();
+            }
+            
+            if (LeftTurn == true)
+            {
+                lblTurn.Text = "Left";
+            }
+            else
+            {
+                lblTurn.Text = "Right";
+            }
+
+            for (int i = 0; i < 6; i++)
+            {
+                LeftSide[i].Text = LeftSideVals[i].ToString();
+                RightSide[i].Text = RightSideVals[i].ToString();
+            }
+
+            lblBottomStore.Text = BottomStore.ToString();
+            lblTopStore.Text = TopStore.ToString();
+        }
+        void CollectAll()
+        {
+            BottomStore += LeftSideVals.Sum();
+            TopStore += RightSideVals.Sum();
+
+            for (int i = 0; i < 6; i++)
+            {
+                LeftSideVals[i] = 0;
+                RightSideVals[i] = 0;
+            }
+        }
+
+        void MoveStones(int loc, bool left)
+        {
+            int opposite = 5 - loc;
+            int stones;
+
+            if (left)
+            {
+                stones = LeftSideVals[loc];
+                LeftSideVals[loc] = 0;
+            }
+            else
+            {
+                stones = RightSideVals[loc];
+                RightSideVals[loc] = 0;
+            }
+            
+            if (left)
+            {
+                int offset = 0;
+                while (stones > 0)
+                {
+                    stones--;
+                    if (loc + offset < 6)
+                    {
+                        LeftSideVals[loc]++;
+                    }
+                    if (loc + offset == 6)
+                    {
+                        BottomStore++;
+                    }
+                    if (loc + offset > 6)
+                    {
+
+                    }
+
+                }
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
